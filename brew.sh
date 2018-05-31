7e50d574fde4b717af8ab413900be22a6cb75cf1
Matt Beall <mbeall@starverte.com>
2018-05-30 22:30:28 -0600

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -22,7 +22,6 @@ brew install gnu-sed --with-default-names
 # Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
 # running `chsh`.
 brew install bash
-brew tap homebrew/versions
 brew install bash-completion
 brew link --overwrite bash-completion
 
@@ -43,45 +42,45 @@ brew install wget --with-iri
 
 # Install more recent versions of some macOS tools.
 brew install vim --with-override-system-vi
-brew install homebrew/dupes/grep
-brew install homebrew/dupes/openssh
-brew install homebrew/dupes/screen
-brew install homebrew/php/php70 --with-gmp --with-fpm
+brew install grep
+brew install openssh
+#brew install screen
+brew install php70 --with-gmp --with-fpm
 
 # Install font tools.
-brew tap bramstein/webfonttools
-brew install sfnt2woff
-brew install sfnt2woff-zopfli
-brew install woff2
+#brew tap bramstein/webfonttools
+#brew install sfnt2woff
+#brew install sfnt2woff-zopfli
+#brew install woff2
 
 # Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
+#brew install aircrack-ng
 #brew install bfg
 brew install binutils
 #brew install binwalk
 #brew install cifer
 #brew install dex2jar
-brew install dns2tcp
+#brew install dns2tcp
 #brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
+#brew install foremost
+#brew install hashpump
+#brew install hydra
+#brew install john
+#brew install knock
+#brew install netpbm
 brew install nmap
-brew install pngcheck
+#brew install pngcheck
 #brew install socat
 brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
+#brew install tcpflow
+#brew install tcpreplay
 #brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install homebrew/x11/xpdf
+#brew install ucspi-tcp # `tcpserver` etc.
+#brew install homebrew/x11/xpdf
 #brew install xz
 
 # Install other useful binaries.
-brew install ack
+#brew install ack
 #brew install dark-mode
 #brew install exiv2
 brew install git
@@ -91,14 +90,14 @@ brew install imagemagick --with-webp
 #brew install lynx
 #brew install p7zip
 #brew install pigz
-brew install pv
-brew install rename
+#brew install pv
+#brew install rename
 #brew install rhino
 brew install speedtest_cli
-brew install ssh-copy-id
+#brew install ssh-copy-id
 brew install testssl
 brew install tree
-brew install vbindiff
+#brew install vbindiff
 brew install webkit2png
 #brew install zopfli
 
@@ -113,8 +112,6 @@ brew install php70-xdebug
 brew install phpunit
 brew install python
 brew install ruby
-brew install homebrew/dupes/srm
-brew link srm --force
 brew install nvm
 
 # Install EditorConfig
